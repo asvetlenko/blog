bf1973b6f9defe4576e8cc79b8f66ca918729d40
Alexey <alexey.svetlenko@gmail.com>
2015-12-20 00:32:02 +0300

    module 'logger1984' version two

--- logger1984/index.js
+++ logger1984/index.js
@@ -5,11 +5,15 @@
 //npm publish
 //npm adduser
 //npm unpublsh
+
+// http://registry.npmjs.org
+//http://registry.npmjs.org/logger1984
+
 module.exports = function (module) {
 
     return {
         log: function () {
-            var args = [module.filename].concat([].slice.call(arguments));
+            var args = ['alexey', module.filename].concat([].slice.call(arguments));
 
             console.log.apply(console, args);
         }
