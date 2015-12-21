f0d1912a63305820a86bd363149b31e97519a31e
Alexey <alexey.svetlenko@itechart-group.com>
2015-12-21 15:02:36 +0300

    easy server was done

--- server.js
+++ server.js
@@ -3,3 +3,19 @@
  */
 
 var http = require('http');
+
+var server = new http.Server(); // EventEmitter
+
+server.listen(1337, '127.0.0.1');
+
+var emit = server.emit;
+server.emit = function (event) {
+    console.log('server event name: ', event);
+    emit.apply(server, arguments);
+}
+
+var counter = 0;
+server.on('request', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.end('Hellow, world!' + ++counter);
+});
