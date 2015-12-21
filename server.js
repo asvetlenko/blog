e0dfd869026093e03adfb67450d7d4978702b30d
Alexey <alexey.svetlenko@gmail.com>
2015-12-21 16:02:33 +0300

    echo server was don with headers

--- server.js
+++ server.js
@@ -2,20 +2,28 @@
  * Created by alexey.svetlenko on 21.12.2015.
  */
 
+// http://127.0.0.1/echo?message=Hello -> Hello
+
+var url = require('url');
 var http = require('http');
 
-var server = new http.Server(); // EventEmitter
+var server = new http.Server(function (req, res) {
+    //res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    //res.end('Hellow, world!');
 
-server.listen(1337, '127.0.0.1');
+    console.log(req.headers);
 
-var emit = server.emit;
-server.emit = function (event) {
-    console.log('server event name: ', event);
-    emit.apply(server, arguments);
-}
-
-var counter = 0;
-server.on('request', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-    res.end('Hellow, world!' + ++counter);
-});
+    var urlParsed = url.parse(req.url, true);
+    if (urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=UTF-8',
+            'Cache-control': 'no-cache, no-store, must-revalidate'
+        });
+        //res.setHeader('Cache-control', 'no-cache, no-store, must-revalidate');
+        res.write('my data to thread. ');
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+}).listen(1337, '127.0.0.1');
