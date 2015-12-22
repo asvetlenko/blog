965680c123f4f5733e1fbc57cde5203c0cad1c92
Alexey <alexey.svetlenko@gmail.com>
2015-12-22 14:05:19 +0300

    io test

--- server.js
+++ server.js
@@ -2,32 +2,38 @@
  * Created by alexey.svetlenko on 21.12.2015.
  */
 
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
 
 var server = new http.Server();
 
 server.on('request', function (req, res) {
-    if (req.url === '/') {
-        fs.readFile('index.html', function (err, info) {
-            if (err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end("Server error!!!!");
-                return;
-            }
 
-            res.end(info);
+    //process.nextTick(function(){
+    //    req.on('readable', function(){
+    //        console.log('Should execute for near data 1!');
+    //        res.end('ok');
+    //    });
+    //});
+
+    var part = 0;
+    setImmediate(function () {
+        console.log('Should execute for near data2');
+        res.end('ok');
         });
-    }
+
 });
 
 server.listen(1337, '127.0.0.1');
 
 var timeoutRef = setTimeout(function () {
     server.close();
+    //server.close(function(){ process.exit(); });
+    //server.close(function(){ clearInterval(intervalRef); });
 }, 2500);
 
 var intervalRef = setInterval(function () {
     console.log('memory usage: ', process.memoryUsage());
 }, 1000);
+
+intervalRef.unref();
+intervalRef.ref();
