0a0ce1e996d9d9018056a2acdeb2b2e3658316d9
Alexey <alexey.svetlenko@gmail.com>
2015-12-27 21:26:42 +0300

    simble message as in lesson

--- app.js
+++ app.js
@@ -23,8 +23,34 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+
+//MY
+app.use(function (req, res, next) {
+    if (req.url === '/') {
+        res.end('Hello World!!! I am Express....!');
+    } else {
+        next();
+    }
+});
+app.use(function (req, res, next) {
+    if (req.url === '/test') {
+        res.end('This is test for Middleware!');
+    } else {
+        next();
+    }
+});
+app.use(function (req, res, next) {
+    if (req.url === '/error') {
+
+        setTimeout(function () {
+            throw new Error('...');
+        }, 10);
+    } else {
+        next();
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -59,3 +85,4 @@ app.use(function (err, req, res, next) {
 
 
 module.exports = app;
+
