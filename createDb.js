80561557e8cc5dac59f32c61f270e6597fe1acb4
Alexey <alexey.svetlenko@gmail.com>
2015-12-29 01:47:26 +0300

    insert test values

---
+++ createDb.js
@@ -0,0 +1,36 @@
+/**
+ * Created by we on 29-Dec-15.
+ */
+
+//cd D:\alexey\MongoDB\Server\3.2\bin\
+//mongod.exe --dbpath "D:\alexey\MongoDB\db" -v
+
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/chat';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a: 2}, function (err, doc) {
+        collection.count(function (err, count) {
+            console.log('coun = %s', count);
+        });
+
+        // Locate all the entries using find
+        collection.find().toArray(function (err, results) {
+            console.dir(results);
+            //Let's close the db
+            db.close();
+        });
+    });
+
+
+    //assert.equal(null, err);
+    //console.log("Connected correctly to server");
+    //
+    //db.close();
+});
